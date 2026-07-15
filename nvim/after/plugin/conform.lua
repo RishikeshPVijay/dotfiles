ced01ee91535affc9f80f158eb8b5a88e844a3eb
Neutrino <neutrino249@gmail.com>
2026-07-15 18:02:42 +0530

    nvim conform latest

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -1,22 +1,22 @@
 require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		javascript = { { "prettierd", "prettier" } },
-		typescript = { { "prettierd", "prettier" } },
-		javascriptreact = { { "prettierd", "prettier" } },
-		typescriptreact = { { "prettierd", "prettier" } },
-		css = { { "prettierd", "prettier" } },
-		scss = { { "prettierd", "prettier" } },
-		json = { { "prettierd", "prettier" } },
-		html = { { "prettierd", "prettier" } },
-		prisma = { { "prisma_fmt" } },
-	},
-	formatters = {
-		prisma_fmt = {
-			command = "npx",
-			args = { "prisma", "format", "--schema", "$FILENAME" },
-			stdin = false,
-		},
-	},
-	format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { "prettierd", "prettier", stop_after_first = true, },
+        typescript = { "prettierd", "prettier", stop_after_first = true, },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true, },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true, },
+        css = { "prettierd", "prettier", stop_after_first = true, },
+        scss = { "prettierd", "prettier", stop_after_first = true, },
+        json = { "prettierd", "prettier", stop_after_first = true, },
+        html = { "prettierd", "prettier", stop_after_first = true, },
+        prisma = { "prisma_fmt", stop_after_first = true, },
+    },
+    formatters = {
+        prisma_fmt = {
+            command = "npx",
+            args = { "prisma", "format", "--schema", "$FILENAME" },
+            stdin = false,
+        },
+    },
+    format_on_save = { timeout_ms = 500, lsp_fallback = true },
 })
